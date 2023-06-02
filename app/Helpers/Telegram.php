28731fc010fb7bc4143dae74d1687bbaed8b7eaf
Ryudenshi <skarydog17@gmail.com>
2023-06-02 20:31:20 +0300

    batabase connected, cURL error 60 fixed

--- app/Helpers/Telegram.php
+++ app/Helpers/Telegram.php
@@ -19,19 +19,60 @@ class Telegram
 
     public function sendMessage($chat_id, $message)
     {
-        return $this->http::post(self::url . $this->bot . '/sendMessage', [
+        return $this->http::withOptions([
+            'verify' => 'C:\xampp\php\cacert.pem',
+        ])->post(self::url . $this->bot . '/sendMessage', [
             'chat_id' => $chat_id,
             'text' => $message,
             'parse_mode' => 'html',
         ]);
     }
 
+    public function editMessage($chat_id, $message, $message_id)
+    {
+        return $this->http::withOptions([
+            'verify' => 'C:\xampp\php\cacert.pem',
+        ])->post(self::url . $this->bot . '/editMessageText', [
+            'chat_id' => $chat_id,
+            'text' => $message,
+            'parse_mode' => 'html',
+            'message_id' => $message_id,
+        ]);
+    }
+
     public function sendDocument($chat_id, $file, $reply_id = null)
     {
-        return $this->http::attach('document', Storage::get('/public/' . $file), 'document.png')
+        return $this->http::withOptions([
+            'verify' => 'C:\xampp\php\cacert.pem',
+        ])->attach('document', Storage::get('public/' . $file), 'document.png')
             ->post(self::url . $this->bot . '/sendDocument', [
                 'chat_id' => $chat_id,
                 "reply_to_message_id" => $reply_id,
             ]);
+    }
+
+    public function sendButtons($chat_id, $message, $button)
+    {
+        return $this->http::withOptions([
+            'verify' => 'C:\xampp\php\cacert.pem',
+        ])->post(self::url . $this->bot . '/sendMessage', [
+            'chat_id' => $chat_id,
+            'text' => $message,
+            'parse_mode' => 'html',
+            'reply_markup' => $button,
+        ]);
+    }
+
+    public function editButtons($chat_id, $button, $message, $message_id)
+    {
+        return $this->http::withOptions([
+            'verify' => 'C:\xampp\php\cacert.pem',
+        ])->post(self::url . $this->bot . '/editMessageText', [
+            'chat_id' => $chat_id,
+            'text' => $message,
+            'parse_mode' => 'html',
+            'reply_markup' => $button,
+            'message_id' => $message_id,
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,30 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Http;
 use app\Http\Controllers\TelegramViberBotController;
 
-Route::post('/telegram/webhook', 'TelegramViberBotController@handleIncomingMessage');
-
-Route::post('/telegram/webhook', 'TelegramViberBotController@handleWebhook');
-
 Route::get('/', function (Telegram $telegram) {
+    $buttons = [
+        'inline_keyboard' => [
+            [
+                [
+                    'text' => 'button4',
+                    'callback_data' => '1'
+                ],
+                [
+                    'text' => 'button2',
+                    'callback_data' => '2'
+                ],
+            ]
+        ]
+    ];
+
+    $sendMessage = $telegram->editButtons(436481135, 'test2', json_encode($buttons), 29);
+    $sendMessage = json_decode($sendMessage);
+    dd($sendMessage);
+});
+
+/*Route::get('/', function (Telegram $telegram) {
     $sendMessage = $telegram->sendMessage(436481135, 'test');
     $sendMessage = json_decode($sendMessage);
-    $http = $telegram->sendDocument(436481135, 'Slider.png', $sendMessage->result->message_id);
+    $http = $telegram->sendDocument(436481135, 'Slider.png', $sendMessage->result->message);
     dd($http->body());
-});
+});*/
